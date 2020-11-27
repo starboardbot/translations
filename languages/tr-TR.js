7b8e56c8cc6a7c1cafebe6ea306055abeb0f5662
Bae <50950966+baen@users.noreply.github.com>
2020-11-27 22:31:43 +0300

    Turkish translation (#1)

---
+++ languages/tr-TR.js
@@ -0,0 +1,19 @@
+module.exports = {
+  name: "Türkçe",
+  HELLO_WORLD: "Merhaba dünya!",
+  
+  // miscellaneous
+  IMAGE: num => `Resim${typeof num === "number" && num ? ` ${num}` : ""}`,
+  GIF_VIDEO: num => `GIF Video${typeof num === "number" && num ? ` ${num}` : ""}`,
+  VIDEO: num => `Video${typeof num === "number" && num ? ` ${num}` : ""}`,
+  MEDIA: num => `Medya${typeof num === "number" && num ? ` ${num}` : ""}`,
+  
+  // starred message embed
+  CLICK_TO_JUMP: deleted =>
+    !deleted ? "Mesaja gitmek için tıklayın!" : "Kaynak (mesaj silinmiş)",
+  SHORT_CLICK_TO_JUMP: deleted =>
+    !deleted ? "Tıkla!" : "Kaynak",
+  REFERENCED_MESSAGE: isReply =>
+    isReply ? "Bu mesajı yanıtlıyor" : "Referans Mesaj",
+  TWEET: (authorName, number) => `Tweet${number && typeof number === "number" ? ` ${number}` : ""} ${authorName} tarafından`,
+}

4ddf2c241da0683f05a7a00099c64840b03539ce
Vexot <69873276+Vexot@users.noreply.github.com>
2022-06-02 19:43:07 -0500

    Removed Old Languages Translations

--- languages/es-ES.js
+++ languages/es-ES.js
@@ -1150,8 +1150,6 @@ module.exports = {
       EN_GB: "Inglés (Reino Unido)",
       EN_US: "Inglés (Estados Unidos)",
       ES_ES: "Español",
-      LT_LT: "Lituano",
-      TR_TR: "Turco",
       FUWWY: "Fuwwy",
     }
   }

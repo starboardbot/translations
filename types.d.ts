8e77a5538759eeb169228d3ac38381aa5be99a5e
TheNoob27 <bwsuperweevil@gmail.com>
2023-10-21 20:49:35 +0100

    settings and overrides commands!!

--- types.d.ts
+++ types.d.ts
@@ -22,6 +22,103 @@ declare type MessageParameters = {
   "SETTINGS_NOT_FOUND": {
     name: string,
   },
+  "SUCCESS_CREATE_SETTINGS": {
+    name: string,
+    type: "channel" | "channelType" | "emoji" | "role",
+    channels: number,
+    emojis: number,
+    roles: number,
+  },
+  "SUCCESS_CREATE_SETTINGS_CHANNELS_NOTICE": {
+    channels: number,
+  },
+  "CONFIRMATION_DELETE_SETTINGS": {
+    name: string,
+  },
+  "SUCCESS_DELETE_SETTINGS": {
+    name: string,
+  },
+  "CONFIRMATION_RESET_CREATED_SETTINGS": {
+    name: string,
+  },
+  "SUCCESS_RESET_SETTINGS": {
+    name: string | null,
+  },
+  "SUCCESS_CREATE_EMOJI_SETTINGS_NOTICE": {
+    emojis: string,
+  },
+  "INHERITS_SETTINGS_FROM": {
+    name: string,
+  },
+  "SETTINGS_APPLY_CHANNELS": {
+    other: number,
+    channels: string,
+  },
+  "SETTINGS_APPLY_CHANNEL_TYPES": {
+    type: string,
+  },
+  "SETTINGS_APPLY_ROLES": {
+    other: number,
+    channels: string | null,
+    roles: string,
+  },
+  "SETTINGS_APPLY_EMOJIS": {
+    emojis: string,
+    channels: string | null,
+  },
+  "NUMBER_STARS": {
+    stars: number,
+  },
+  "NUMBER_CHARACTERS": {
+    chars: number,
+  },
+  "CANNOT_SEND_MESSAGES_IN_CHANNEL": {
+    channel: string,
+  },
+  "CANNOT_SEND_EMBEDS_IN_CHANNEL": {
+    channel: string,
+  },
+  "CANNOT_VIEW_CHANNEL": {
+    channel: string,
+  },
+  "CANNOT_VIEW_VOICE_CHANNEL": {
+    channel: string,
+  },
+  "CANNOT_READ_HISTORY_IN_CHANNEL": {
+    channel: string,
+  },
+  "SUCCESS_CHANGE_SETTINGS": {
+    name: string,
+  },
+  "SUCCESS_CHANGE_SETTINGS_NOTICE": {
+    notices: string,
+  },
+  "MISSING_ATTACH_FILES_PERMISSION_NOTICE": {
+    channel: string,
+  },
+  "REQUIRED_STARS_CLASH_SET": {
+    requiredStars: number,
+    requiredToRemove: number,
+  },
+  "UNUSABLE_REACTION_EMOJIS_PROVIDED": {
+    emojis: string,
+  },
+  "SETTING_ONLY_FOR_SERVER_SETTINGS": {
+    setting: string,
+  },
+  "SUCCESS_EDIT_SETTINGS": {
+    name: string,
+    changes: string,
+  },
+  "SETTINGS_NOW_NAMED": {
+    name: string,
+  },
+  "SETTINGS_PRIORITY_NOW": {
+    priority: string,
+  },
+  "SETTINGS_NOW_INHERITS": {
+    name: string,
+  },
 }
 
 export = MessageParameters
